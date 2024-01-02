7fcc7b2e44c13aaf30f7119077c2707108b365a7
Uthayamurthy <uthayamurthy2006@gmail.com>
2024-01-02 19:21:02 +0530

    Add license notice

--- passwd_change.py
+++ passwd_change.py
@@ -1,3 +1,24 @@
+'''
+T.U.R.P.O (Trustworthy Utility for Reliable Password Organisation) - A simple to use, feature-rich password management software. 
+
+    Copyright (C) 2023  R Uthaya Murthy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact Author : uthayamurthy2006@gmail.com
+'''
+
 import ttkbootstrap as ttk
 from ttkbootstrap import dialogs
 from ttkbootstrap.constants import *
